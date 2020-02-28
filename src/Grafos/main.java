09bde96da4de9628ab126685ae3736ce94093346
estefania <e.nieto@utp.edu.co>
2020-02-27 20:13:57 -0500

    clases grafos

--- src/Grafos/main.java
+++ src/Grafos/main.java
@@ -1,35 +1,62 @@
 package Grafos;
 
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class main {
     public static void main(String[] args) {
-        nodoGrafo E = new nodoGrafo("E");
         nodoGrafo S = new nodoGrafo("S");
-        nodoGrafo T = new nodoGrafo("T");
+        nodoGrafo C = new nodoGrafo("C");
+        nodoGrafo E = new nodoGrafo("E");
         nodoGrafo F = new nodoGrafo("F");
-        E.agregarHijo(S);
-        E.agregarHijo(T);
-        E.agregarHijo(F);
+        nodoGrafo B = new nodoGrafo("B");
+        nodoGrafo D = new nodoGrafo("D");
         nodoGrafo A = new nodoGrafo("A");
-        F.agregarHijo(A);
-        nodoGrafo N = new nodoGrafo("N");
-        A.agregarHijo(N);
-        nodoGrafo I = new nodoGrafo("I");
-        S.agregarHijo(I);
-
-        // busquedaXAnchura("A",E);
-        busquedaXProfundidad("A",E);
+        nodoGrafo Z = new nodoGrafo("Z");
+        nodoGrafo G = new nodoGrafo("G");
+        S.agregarHijo(C);
+        S.agregarHijo(E);
+        S.agregarHijo(F);
+        C.agregarHijo(B);
+        C.agregarHijo(E);
+        C.agregarHijo(S);
+        C.agregarHijo(A);
+        E.agregarHijo(C);
+        E.agregarHijo(S);
+        F.agregarHijo(B);
+        F.agregarHijo(D);
+        B.agregarHijo(C);
+        B.agregarHijo(F);
+        B.agregarHijo(D);
+        B.agregarHijo(Z);
+        B.agregarHijo(A);
+        D.agregarHijo(Z);
+        D.agregarHijo(F);
+        D.agregarHijo(B);
+        A.agregarHijo(C);
+        A.agregarHijo(B);
+        A.agregarHijo(G);
+        Z.agregarHijo(B);
+        Z.agregarHijo(D);
+        Z.agregarHijo(G);
+        G.agregarHijo(A);
+        G.agregarHijo(Z);
+        busquedaXAnchura("G",S);
+        //busquedaXProfundidad("A",E);
     }
     public static boolean busquedaXAnchura (String valor, nodoGrafo raiz){
+        Vector  mivector = new Vector();
+
         Queue<nodoGrafo> colaAux= new LinkedList<nodoGrafo>();
         colaAux.add(raiz);
         while(colaAux.size()!=0){
             nodoGrafo getNode =colaAux.poll();
+
             System.out.println(getNode.valor);
             getNode.estado=true;
+            mivector[i] = getNode;
             if(getNode.valor.equals(valor)){
                 System.out.println("nodoGrafo Encontrado: "+getNode.valor);
                 return true;
@@ -37,7 +64,10 @@ public class main {
             else
             {
                 for (nodoGrafo n: getNode.noditos) {
-                    if(getNode.estado=true)
+                    for (int i = 0; i < mivector.length; i++) {
+
+                    }
+                    if(n.estado==false)
                     colaAux.add(n);
                 }
             }
